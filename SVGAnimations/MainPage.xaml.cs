8911ce680d514ef2c3ecdea51779b7a66b51edef
Juan Pablo <juanpablogc@gmail.com>
2017-03-02 16:35:24 +0000

    Add project files.

---
+++ SVGAnimations/MainPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using SVGAnimations.Manager;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace SVGAnimations
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            this.Loaded += MainPage_Loaded;
+        }
+
+        private SVGAnimator svg;
+
+        private async void MainPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            svg = new SVGAnimator();
+            await svg.Initialize("ms-appx:///Assets/Svg/rectangle.svg");
+            SvgContainer.Children.Add(svg.Image);
+
+
+            svg.StartAnimation();
+        }
+    }
+}
---
+++ SVGAnimations/Manager/SVGManager.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
+using Windows.Storage.Streams;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media.Imaging;
+
+namespace SVGAnimations.Manager
+{
+    public class SVGAnimator
+    {
+        public Image Image { get; private set; }
+        private SvgImageSource svgsource;
+
+        public SVGAnimator()
+        {
+            Image = new Image();
+            svgsource = new SvgImageSource();
+            Image.Source = svgsource;
+        }
+
+        private string svgData;
+        IRandomAccessStream memoryStream;
+        public async Task Initialize(string urisource)
+        {
+            var file = await StorageFile.GetFileFromApplicationUriAsync(new Uri(urisource));
+            svgData = await FileIO.ReadTextAsync(file);
+
+            memoryStream = new InMemoryRandomAccessStream();
+            await Write(memoryStream,svgData);
+
+            await Refresh();
+        }
+
+        private async Task Refresh()
+        {
+            memoryStream.Seek(0);
+            await svgsource.SetSourceAsync(memoryStream);
+        }
+
+        private async Task Write(IRandomAccessStream stream, string content)
+        {
+            memoryStream.Seek(0);
+            using (var dataWriter = new DataWriter(stream))
+            {
+                dataWriter.WriteString(content);
+                await dataWriter.StoreAsync();
+                await dataWriter.FlushAsync();
+                dataWriter.DetachStream();
+            }
+        }
+
+        public void StartAnimation()
+        {
+            if (svgData == null)
+                return;
+
+            AnimateSvg();
+        }
+
+        int strokewidth = 1;
+        private async Task AnimateSvg()
+        {
+            svgData = svgData.Replace($"stroke-width:{strokewidth};", $"stroke-width:{strokewidth + 1};");
+            strokewidth++;
+            await Write(memoryStream, svgData);
+            await Refresh();
+            await Task.Delay(1000);
+            AnimateSvg();
+        }
+    }
+}
